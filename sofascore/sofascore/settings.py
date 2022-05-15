fd7a2a06439b7ded41b43b68ab11bdffece0457a
leo shang <leo.shang@gmail.com>
2022-05-15 22:30:50 +0200

    FIX homeScore & awayScore exception. The dictionary needs to be at first if its empty!

--- sofascore/sofascore/settings.py
+++ sofascore/sofascore/settings.py
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'sofascore.pipelines.SofascorePipeline': 300,
+    'sofascore.pipelines.SofascorePipeline': 100,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
--- sofascore/sofascore/spiders/sofa_score_spider.py
+++ sofascore/sofascore/spiders/sofa_score_spider.py
@@ -21,42 +21,42 @@ class SofaScoreCrawler(scrapy.Spider):
     def __init__(self, *a, **kw):
         super(SofaScoreCrawler, self).__init__(*a, **kw)
         self.start_urls = []
-        self.votes_inspector = SofaVotesInspector()
-        self.round_count = 1
 
-        round_link = SofaScoreConfiguration.get_round_link(self.round_count)
-        self.start_urls.append(round_link)
-        print(self.start_urls)
+        for round_count in range(1, 39):
+            round_link = SofaScoreConfiguration.get_round_link(round_count)
+            self.start_urls.append(round_link)
+        # print(self.start_urls)
 
     def parse(self, response):
         # print response.request.headers['User-Agent']
         # print response.request.headers.get('Referrer', None)
-        print response.url
+
         all_match_data = json.loads(response.body.encode(_UTF_8_))
         data1_array = all_match_data['events']
 
         for match in data1_array:
             score_item = SofascoreItem()
+            # print json.dumps(match)
             score_item['match_id'] = match['id']
             score_item['startTimestamp'] = datetime.utcfromtimestamp(match['startTimestamp']).strftime('%Y-%m-%d %H:%M:%S')
             score_item['hometeam'] = match['homeTeam']['name']
             score_item['hometeam_fans'] = match['homeTeam']['userCount']
-            score_item['hometeam_score'] = match['homeScore']['normaltime']
+
+            if match['homeScore']:
+                score_item['hometeam_score'] = match['homeScore']['normaltime']
+            else:
+                print str(match['id']) + ':' + str(match['roundInfo']['round'])\
+                      + ':' + match['homeTeam']['name'] + ':' + match['awayTeam']['name']
+            if match['awayScore']:
+                score_item['guestteam_score'] = match['awayScore']['normaltime']
 
             score_item['guestteam'] = match['awayTeam']['name']
             score_item['guestteam_fans'] = match['awayTeam']['userCount']
-            score_item['guestteam_score'] = match['awayScore']['normaltime']
 
             match_vote_url = SofaScoreConfiguration.get_vote_site().replace('$event_id', str(match['id']))
-            print match_vote_url
+            # print 'vote url: ' + match_vote_url
 
-            self.round_count += 1
-            next_round_url = SofaScoreConfiguration.get_season_round_site().replace('$round_number',
-                                                                                    str(self.round_count))
-
-            request_event = scrapy.Request(match_vote_url, callback=self.votes_inspector.extract_votes,
-                                           meta={'score_item': score_item,
-                                                 'next_round': self.round_count,
-                                                 'next_round_url': next_round_url})
+            request_event = scrapy.Request(match_vote_url, callback=SofaVotesInspector.extract_votes,
+                                           meta={'score_item': score_item})
             yield request_event
 
