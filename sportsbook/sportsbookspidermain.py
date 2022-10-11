1c31d87c943aac179db8fb329ee859d7b1626b85
Leo Shang <leo.shang@gmail.com>
2022-10-11 10:35:22 +0200

    reversed

--- sportsbook/sportsbookspidermain.py
+++ sportsbook/sportsbookspidermain.py
@@ -1,3 +1,2 @@
 from scrapy import cmdline
-#cmdline.execute("scrapy crawl sportsbookspider -s FEED_EXPORT_ENCODING=utf-8".split())
-cmdline.execute("scrapy crawl asianoddsspider -s FEED_EXPORT_ENCODING=utf-8".split())
+cmdline.execute("scrapy crawl sportsbookspider -s FEED_EXPORT_ENCODING=utf-8".split())
